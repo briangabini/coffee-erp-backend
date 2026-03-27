8cad225743b60d2c460055d913b2fd98a9c3890b
brngbn <brngbncoding@gmail.com>
2026-03-27 14:11:33 +0800

    refactor

--- src/main/java/com/briangabini/coffee_erp_backend/web/dto/ValidationMessages.java
+++ src/main/java/com/briangabini/coffee_erp_backend/web/dto/ValidationMessages.java
@@ -6,12 +6,14 @@ public final class ValidationMessages {
         throw new UnsupportedOperationException("Utility class");
     }
 
+    // --- Common Validation Messages ---
+    public static final String MUST_BE_GREATER_THAN_OR_EQUAL_TO_0 = "must be greater than or equal to 0";
+
     // --- Coffee Bean Messages ---
     public static final String BEAN_NAME_REQUIRED = "Bean name is required";
     public static final String ORIGIN_REQUIRED = "Origin is required";
     public static final String ROAST_LEVEL_REQUIRED = "Roast level is required";
     public static final String PRICE_REQUIRED = "Price per kg is required";
-    public static final String PRICE_NEGATIVE = "must be greater than or equal to 0"; // Default hibernate message
 
     // --- Inventory Stock Messages ---
     public static final String COFFEE_BEAN_ID_REQUIRED = "Coffee Bean ID is required";
