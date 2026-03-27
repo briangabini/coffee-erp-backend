eb1fdf17f1328232093d475800111a994d5dbffd
brngbn <brngbncoding@gmail.com>
2026-03-27 13:43:56 +0800

    feat: handle MethodArgumentNotValidException

--- src/main/java/com/briangabini/coffee_erp_backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/briangabini/coffee_erp_backend/exceptions/GlobalExceptionHandler.java
@@ -4,10 +4,14 @@ import com.briangabini.coffee_erp_backend.web.dto.ApiErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -25,5 +29,20 @@ public class GlobalExceptionHandler {
         );
 
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        log.warn("Validation failed: {}", errors);
+        return ResponseEntity.badRequest().body(errors);
     }
 }
