9cfcd7025cdfe858ac20beb2d48be7e50a7a98b6
brngbn <brngbncoding@gmail.com>
2026-03-28 12:22:34 +0800

    fix: fix syntax issues

--- src/main/resources/db/migration/V3__init_security_tables.sql
+++ src/main/resources/db/migration/V3__init_security_tables.sql
@@ -2,13 +2,13 @@
 CREATE TABLE authority
 (
     id         UUID PRIMARY KEY NOT NULL,
-    permission VARCHAR(255),
+    permission VARCHAR(255)
 );
 
 CREATE TABLE role
 (
     id   UUID PRIMARY KEY NOT NULL,
-    name VARCHAR(255),
+    name VARCHAR(255)
 );
 
 CREATE TABLE users
@@ -19,7 +19,7 @@ CREATE TABLE users
     account_non_expired     BOOLEAN DEFAULT TRUE,
     account_non_locked      BOOLEAN DEFAULT TRUE,
     credentials_non_expired BOOLEAN DEFAULT TRUE,
-    enabled                 BOOLEAN DEFAULT TRUE,
+    enabled                 BOOLEAN DEFAULT TRUE
 );
 
 -- Associations
@@ -27,16 +27,16 @@ CREATE TABLE role_authority
 (
     authority_id UUID NOT NULL,
     role_id      UUID NOT NULL,
-    PRIMARY KEY (authority_id, role_id)
+    PRIMARY KEY (authority_id, role_id),
     CONSTRAINT fk_role_auth_role FOREIGN KEY (role_id) REFERENCES role (id),
-    CONSTRAINTS fk_role_auth_auth FOREIGN KEY (authority_id) REFERENCES authority (id)
+    CONSTRAINT fk_role_auth_auth FOREIGN KEY (authority_id) REFERENCES authority (id)
 );
 
 CREATE TABLE user_role
 (
     role_id UUID NOT NULL,
     user_id UUID NOT NULL,
-    PRIMARY KEY (role_id, user_id)
+    PRIMARY KEY (role_id, user_id),
     CONSTRAINT fk_user_role_role FOREIGN KEY (role_id) REFERENCES role (id),
     CONSTRAINT fk_user_role_user FOREIGN KEY (user_id) REFERENCES users (id)
 );
