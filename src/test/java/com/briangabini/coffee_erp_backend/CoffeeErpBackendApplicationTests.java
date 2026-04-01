c937584044c7bf0a8c8c0a177e87b4f45eac1301
brngbn <brngbncoding@gmail.com>
2026-04-01 09:16:17 +0800

    fix: fix CoffeeErpBackendApplicationTests by adding a property

--- src/test/java/com/briangabini/coffee_erp_backend/CoffeeErpBackendApplicationTests.java
+++ src/test/java/com/briangabini/coffee_erp_backend/CoffeeErpBackendApplicationTests.java
@@ -5,7 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(properties = {
+		"application.security.jwt.secret-key=404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970"
+})
 class CoffeeErpBackendApplicationTests {
 
 	@Test
