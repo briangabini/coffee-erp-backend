d35ff06febc85779c926aebf3f1e57b419bc10f0
brngbn <brngbncoding@gmail.com>
2026-04-01 12:37:23 +0800

    feat: add Permissions

---
+++ src/main/java/com/briangabini/coffee_erp_backend/security/permissions/SupplierUpdatePermission.java
@@ -0,0 +1,14 @@
+package com.briangabini.coffee_erp_backend.security.permissions;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasAuthority('supplier.update')")
+public @interface SupplierUpdatePermission {
+}
